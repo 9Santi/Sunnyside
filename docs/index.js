e8092422926f16b2a0b034cbe939c230ab7a0906
Santi <32875016+9Santi@users.noreply.github.com>
2021-09-21 20:11:14 +0200

    Add files via upload

---
+++ docs/index.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('.navbar')
+const toggle = document.querySelector('.toggle')
+
+navbar.addEventListener('click', () =>{
+    navbar.classList.toggle('active')
+    
+ 
+})
+
+
